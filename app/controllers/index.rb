e8dc5b41ced33fb0d30cd116334837e5d780330c
Chris Camp <cpcamp2@gmail.com>
2017-09-28 14:28:14 -0400

    Ajaxified this app

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,5 +7,9 @@ end
 post '/rolls' do
   @die = Die.new(params[:sides].to_i)
 
-  erb :index  # HINT: what does this do? what should we do instead?
+  if request.xhr?
+  	erb :'_die', layout: false
+  else
+  	erb :index  # HINT: what does this do? what should we do instead?
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -2,8 +2,23 @@ $(document).ready(function () {
 
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- use jQuery to submit an AJAX post to the form's action
-  //   4- when the AJAX post is done, display the new die roll using jQuery
+	$('#action-form').on('submit', function(event){
 
-});
+  //   2- prevent the default action for that event from happening
+  	event.preventDefault();
+
+  //   3- use jQuery to submit an AJAX post to the form's action
+  	var $form = $(this);
+  	var request = $.ajax({
+  		url: $form.attr('action'),
+  		method: $form.attr('method'),
+  		data: $form.serialize()
+  	});
+  	  //   4- when the AJAX post is done, display the new die roll using jQuery
+  	  request.done(function(response){
+  	  	console.log(response);
+  	  	$('#die-container').html(response);
+  	  });
+  	});
+  });
+
