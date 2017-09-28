d50ed1b65d04d9dbb65f23eeb3d11c1b0226b81f
Andreas Bach Landgrebe <andreaslandgrebe@gmail.com>
2017-09-28 16:49:56 -0400

    add ajax to roll dice.

--- public/js/application.js
+++ public/js/application.js
@@ -6,4 +6,24 @@ $(document).ready(function () {
   //   3- use jQuery to submit an AJAX post to the form's action
   //   4- when the AJAX post is done, display the new die roll using jQuery
 
+  $("#form-roll-dice").on("submit", function(event) {
+    event.preventDefault();
+
+    var $form = $(event.target);
+
+    var action = $form.attr("action");
+
+    var method = $form.attr("method");
+
+    var data = $form.serialize();
+
+    $.ajax({
+      method: method,
+      url: action,
+      data: data
+    })
+    .done(function(response) {
+      $("#die-container").html(response);
+    })
+  })
 });
