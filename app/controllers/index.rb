8564d44d2f81d5e8f32bfb1915697d4cffb924be
Phil Sofia <phil.sofia@gmail.com>
2017-09-28 12:19:41 -0400

    One way to do it, with comments

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,6 +6,17 @@ end
 # TODO: convert this route to use AJAX
 post '/rolls' do
   @die = Die.new(params[:sides].to_i)
-
-  erb :index  # HINT: what does this do? what should we do instead?
+  
+  if request.xhr?
+    # fyi this will also send back appropriate HTML with our response:
+    # "<div class='die'><span class='roll'>#{@die.roll}</span></div>"
+    
+    # but we want to follow the MVC pattern,
+    # so we put our view code in a separate file and call it here:
+    # notice we add the "layout: false" option to avoid sending
+    # the layout code back also (which is Sinatra's default behavior)
+    erb :_roll, layout: false
+  else
+    erb :index
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -5,5 +5,34 @@ $(document).ready(function () {
   //   2- prevent the default action for that event from happening
   //   3- use jQuery to submit an AJAX post to the form's action
   //   4- when the AJAX post is done, display the new die roll using jQuery
-
+  
+    // with this event listener we can control what happens
+    // when this specific form is submitted:
+    $("#die-roll-post-form").on("submit", function(event) {
+        event.preventDefault();
+        
+        // let's grab the form as a jQuery object,
+        // so we can call jQuery methods on it:
+        var $form = $(event.target);
+        
+        // now let's get what we need from the form:
+        // we need the method, the url, and the data
+        var url = $form.attr("action");
+        var method = $form.attr("method");
+        // what does this serialize() method do when called on a form?
+        var data = $form.serialize();
+        
+        // now let's take what we got from the form
+        // and generate & send an asych HTTP request via $.ajax:
+        $.ajax({
+            method: method,
+            url: url,
+            data: data
+        })
+        .done(function(response) {
+            // in here we will have the response from the server
+            // which we can then place on the DOM where we want it to be:
+            $("#die-container").html(response);
+        })
+    })
 });
