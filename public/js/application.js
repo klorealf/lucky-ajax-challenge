019b7361371b01d6ddb99ebf1e61b6f46bfd75cb
Najah Izquierdo <najah.izquierdo@yahoo.com>
2017-09-28 16:04:26 -0400

    Ajaxify

--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
+  $("#die_roll_post").on("submit", function(event) {
 
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- use jQuery to submit an AJAX post to the form's action
-  //   4- when the AJAX post is done, display the new die roll using jQuery
+    event.preventDefault()
+
+    var $form = $(event.target)
+     var url = $form.attr("action");
+     var method = $form.attr("method");
+     var data = $form.serialize();
+
+    $.ajax({
+      url: url,
+      method: method,
+      data: data
+
+    })
+
+    .done(function(response) {
+
+      $("#die").children("span").text(response);
+    })
+  })
 
 });
