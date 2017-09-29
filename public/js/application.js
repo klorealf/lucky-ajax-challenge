5c26992138d9c6ea15c809f3e8ff6a7e33fef76c
Catherine Rice <clr2107@gmail.com>
2017-09-28 20:26:53 -0400

    Add ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,19 @@
-$(document).ready(function () {
+$(document).ready(function() {
+ $("#form_id").on("submit", function(event){
+  event.preventDefault();
+});
 
+ $.ajax ({
+  url: " index.erb"
+
+
+ }) .done(function() {
+  $(this)
+ });
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
   //   2- prevent the default action for that event from happening
   //   3- use jQuery to submit an AJAX post to the form's action
   //   4- when the AJAX post is done, display the new die roll using jQuery
 
-});
+
