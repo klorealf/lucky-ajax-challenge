543132c7af812b73a045529ee80e6edb51354ca9
khadija <email": null>
2017-10-17 12:03:09 -0400

    ajax challenge

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,9 @@
 $(document).ready(function () {
 
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- use jQuery to submit an AJAX post to the form's action
-  //   4- when the AJAX post is done, display the new die roll using jQuery
+  $('#die_roll_form').on('submit', function(event) {
+    event.preventDefault();
 
-  $('#die_roll_form').on('submit', function(e) {
-    e.preventDefault();
-
-    var $form = $(e.target);
+    var $form = $(event.target);
     var url = $form.attr("action");
     var method = $form.attr("method");
     var data = $form.serialize();
